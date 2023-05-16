5c7c071a6f43301e977a70172d85e6ecd8d2d1cb
Susan Sattich <sisattich@gmail.com>
2023-05-16 17:49:32 -0600

    Adding tests for what I want AlbumImage to look like and cleaning up App.test.js a bit.

---
+++ src/MockData.js
@@ -0,0 +1,41 @@
+function MockData() {
+  return [
+    {
+      albumId: 1,
+      id: 1,
+      thumbnailUrl: "thumbs.com/1",
+      title: "Photo 1",
+      url: "photos.com/1",
+    },
+    {
+      albumId: 1,
+      id: 2,
+      thumbnailUrl: "thumbs.com/2",
+      title: "Photo 2",
+      url: "photos.com/2",
+    },
+    {
+      albumId: 2,
+      id: 3,
+      thumbnailUrl: "thumbs.com/3",
+      title: "Photo 3",
+      url: "photos.com/3",
+    },
+    {
+      albumId: 2,
+      id: 4,
+      thumbnailUrl: "thumbs.com/4",
+      title: "Photo 4",
+      url: "photos.com/4",
+    },
+    {
+      albumId: 3,
+      id: 5,
+      thumbnailUrl: "thumbs.com/5",
+      title: "Photo 5",
+      url: "photos.com/5",
+    },
+  ];
+}
+
+export default MockData;
