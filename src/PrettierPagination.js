cd4fdad5fbeabc793db801bb8feea85f31addc73
Susan Sattich <sisattich@gmail.com>
2023-05-18 13:55:31 -0600

    Size pagination component according screen size.

--- src/PrettierPagination.js
+++ src/PrettierPagination.js
@@ -1,6 +1,18 @@
 import { Pagination } from "@mui/material";
+import { useState } from "react";
 
 function PrettierPagination({ count, page, onChange }) {
+  const calculateSize = () =>
+    window.innerWidth < 600
+      ? "small"
+      : window.innerWidth < 1280
+      ? "medium"
+      : "large";
+
+  const [size, setSize] = useState(calculateSize());
+
+  window.addEventListener("resize", () => setSize(calculateSize()));
+
   return (
     <div
       style={{
@@ -15,6 +27,7 @@ function PrettierPagination({ count, page, onChange }) {
         shape="rounded"
         page={page}
         onChange={onChange}
+        size={size}
       />
     </div>
   );
