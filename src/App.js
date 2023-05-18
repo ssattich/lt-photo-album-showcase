84005c2ca13e4a4bbc759df2c9d89e00071acb26
Susan Sattich <sisattich@gmail.com>
2023-05-18 12:23:10 -0600

    Error handling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { TextField } from "@mui/material";
 import AlbumSelectionPages from "./AlbumSelectionPages";
 import PhotosPages from "./PhotosPages";
 
 function App() {
+  const PHOTO_SITE_NAME = "jsonplaceholder.typicode.com/photos";
+  const FETCH_PHOTOS_FROM = "https://" + PHOTO_SITE_NAME;
+
   // TODO: clickable photos
   const [photos, setPhotos] = useState([]);
   const [photosToDisplay, setPhotosToDisplay] = useState([]);
@@ -15,11 +18,14 @@ function App() {
   const [searchedAlbumId, setSearchedAlbumId] = useState("");
   const [selectedAlbumId, setSelectedAlbumId] = useState(null);
 
+  let photosFetchFailed = useRef(false);
+
   useEffect(() => {
     const getAllPhotos = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/photos"
-      ); // TODO: error handling
+      const response = await fetch(FETCH_PHOTOS_FROM).catch(() => {
+        photosFetchFailed.current = true;
+      });
+      if (photosFetchFailed.current) return;
       const json = await response.json();
       setPhotos(json);
       setPhotosFetched(true);
@@ -54,7 +60,13 @@ function App() {
     }
   }, [albumIds, searchedAlbumId]);
 
-  return (
+  return photosFetchFailed.current ? (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <p>We were unable to fetch the photos from {PHOTO_SITE_NAME}.</p>
+      <p>💔</p>
+      <p>Refresh this page or try again later.</p>
+    </div>
+  ) : (
     <>
       <div
         style={{
